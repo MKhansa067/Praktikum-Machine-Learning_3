f300b207edfb36d441b342a03586093ecbdc84d8
MKhansa067 <105094399+MKhansa067@users.noreply.github.com>
2025-04-23 09:29:27 +0700

    Add files via upload

---
+++ DataCleaning.py
@@ -0,0 +1,35 @@
+# %%
+import pandas as pd
+from IPython.display import display
+
+# %%
+# reading the database
+data = pd.read_csv("students.csv")
+
+# %%
+# printing the top 10 rows
+display(data.head(10))
+
+# %%
+# Show the dataset info
+data.info()
+
+# %%
+# Missing Value Checking
+data.isna().sum()
+
+# %%
+# Filling Nan Value with mode
+data['lunch'].fillna(data['lunch'].mode()[0], inplace=True)
+
+#%%
+# Filling Nan Value with mean
+data['reading score'].fillna(data['reading score'].mean(), inplace=True)
+
+# %%
+# Filling Nan Value with mode
+data['grade'].fillna(data['grade'].median()[0], inplace=True)
+
+# %%
+# Check the information of the dataset
+data.info()
